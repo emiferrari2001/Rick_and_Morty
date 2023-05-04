a37447d2f11e2f0c0d7c9b144f89e59c6f98904a
emiferrari2001 <emiferrari2001@gmail.com>
2023-05-04 11:00:26 -0300

    orm i

--- Server/test/index.test.js
+++ Server/test/index.test.js
@@ -10,22 +10,54 @@ describe('Test de RUTAS', ()=>{
         });
         it('Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image"', async ()=>{
             const res = await agent.get('/rickandmorty/character/2');
-            expect(res.body).toHaveProperty('id');
-            expect(res.body).toHaveProperty('name');
-            expect(res.body).toHaveProperty('species');
-            expect(res.body).toHaveProperty('gender');
-            expect(res.body).toHaveProperty('status');
-            expect(res.body).toHaveProperty('origin');
-            expect(res.body).toHaveProperty('image');
+            // expect(res.body).toHaveProperty('id');
+            // expect(res.body).toHaveProperty('name');
+            // expect(res.body).toHaveProperty('species');
+            // expect(res.body).toHaveProperty('gender');
+            // expect(res.body).toHaveProperty('status');
+            // expect(res.body).toHaveProperty('origin');
+            // expect(res.body).toHaveProperty('image');
 
             const res2 = await agent.get('/rickandmorty/character/9');
-            expect(res2.body).toHaveProperty('id');
+            /* expect(res2.body).toHaveProperty('id');
             expect(res2.body).toHaveProperty('name');
             expect(res2.body).toHaveProperty('species');
             expect(res2.body).toHaveProperty('gender');
             expect(res2.body).toHaveProperty('status');
             expect(res2.body).toHaveProperty('origin');
             expect(res2.body).toHaveProperty('image');
+            */
+
+            /*SOLUCION 2
+            
+            const obj = {
+                id: 342,
+                name: 'Emi',
+                species: 'Human',
+                origin: {
+                    name: 'Earth'
+                },
+                image: 'image.jpg',
+                gender: 'Female',
+                status: 'Alive (barely)'
+            }
+            for(const prop in obj){
+                //creo un objeto con las props que quiero, recorro sus props
+                // con for in y lo comparo con el objeto del body
+                expect(res.body).toHaveProperty(prop);
+            }
+            
+            */
+
+            //SOLUCION 3
+
+            /* creo un array con las props que quiero y recorro con forEach
+            voy testeando mi personaje para comparar que tenga las props*/
+            const props = ['id', 'name', 'species', 'gender', 'status', 'origin', 'image'];
+            props.forEach(prop=>{
+                expect(res.body).toHaveProperty(prop)
+                expect(res2.body).toHaveProperty(prop)
+            })
         })
         it('Si hay un error responde con status: 500', async ()=>{
             const res = await agent.get('/rickandmorty/character/930');
@@ -60,7 +92,14 @@ describe('Test de RUTAS', ()=>{
             
             const {body} = await agent.post('/rickandmorty/fav').send(pj1.body);    
             console.log(body)  
-            expect(body).toBeInstanceOf(Array);      
+            expect(body).toBeInstanceOf(Array);   
+            
+            /* Tambien puedo crear un objeto propio con la info que necesito, o pasar el objeto 
+            por post y verificar si el body contiene un equal
+            const pj1 = await agent.get('/rickandmorty/character/12');
+            const response = await agent.post('/rickandmorty/fav').send(pj1.body);
+            expect(response.body).toContainEqual(character);
+            */
         })
         it('Si se envia un segundo personaje, se agrega al arreglo anterior', async() => {
             const pj2 = await agent.get('/rickandmorty/character/17');
